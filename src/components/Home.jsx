68c037dd7383a3bfdee753461b153365b00ab231
Elliot Akande <elbo.lynch@gmail.com>
2023-11-16 13:12:21 +0000

    Add basic dom structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Home(props) {
-  return <div>Home</div>;
+  return (
+    <main>
+      <div>Store Name</div>
+      <h1>THE HUGE BLACK FRIDAY SALE</h1>
+      <Link to="/products">SHOP NOW</Link>
+    </main>
+  );
 }
 
 Home.propTypes = {};
