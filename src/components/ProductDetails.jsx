2678dcc1fd13b49fccda3e8f21d9d4069621004b
Elliot Akande <elbo.lynch@gmail.com>
2023-11-16 14:07:34 +0000

    Add DOM elements

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
+import { useOutletContext, useParams } from "react-router-dom";
 
-function ProductDetails(props) {
-  return <div>ProductDetails</div>;
+function ProductDetails() {
+  const id = parseInt(useParams().id, 10);
+  const { products } = useOutletContext();
+  const product = products ? products.find((item) => item.id === id) : null;
+
+  return (
+    <main>
+      {product && (
+        <>
+          <img src={product.image} alt={product.title} />
+          <div>
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <strong>£{product.price}</strong>
+            <button>Add to cart</button>
+          </div>
+        </>
+      )}
+    </main>
+  );
 }
-
-ProductDetails.propTypes = {};
 
 export default ProductDetails;
