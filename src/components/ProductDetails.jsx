497f08cdb5315b71625173708187b859b12b8759
Elliot Akande <elbo.lynch@gmail.com>
2023-11-16 15:38:35 +0000

    Add tracking of product amount

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,9 +1,24 @@
 import { useOutletContext, useParams } from "react-router-dom";
+import { useState } from "react";
 
 function ProductDetails() {
+  const [amount, setAmount] = useState(1);
+  const { products, cartManager } = useOutletContext();
+
   const id = parseInt(useParams().id, 10);
-  const { products } = useOutletContext();
   const product = products ? products.find((item) => item.id === id) : null;
+
+  const handleAmountChange = (e) => {
+    setAmount(parseInt(e.target.value, 10));
+  };
+
+  const incrementAmount = () => {
+    setAmount((prev) => prev + 1);
+  };
+
+  const decrementAmount = () => {
+    setAmount((prev) => prev - 1);
+  };
 
   return (
     <main>
@@ -14,6 +29,16 @@ function ProductDetails() {
             <h1>{product.title}</h1>
             <p>{product.description}</p>
             <strong>£{product.price}</strong>
+            <div>
+              <button onClick={decrementAmount}>-</button>
+              <input
+                type="number"
+                name="amount"
+                value={amount}
+                onChange={handleAmountChange}
+              />
+              <button onClick={incrementAmount}>+</button>
+            </div>
             <button>Add to cart</button>
           </div>
         </>
