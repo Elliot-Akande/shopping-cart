37d265111db5a309ee4280cdbff20bd0eec5ec14
Elliot Akande <elbo.lynch@gmail.com>
2023-11-17 16:32:41 +0000

    Add styling to product details page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,9 @@
 import { useOutletContext, useParams } from "react-router-dom";
 import { useState } from "react";
 import ProductAmountControl from "../ProductAmountControl/ProductAmountControl";
+import styles from "./ProductDetails.module.css";
+import Price from "../Price/Price";
+import Rating from "../Rating/Rating";
 
 function ProductDetails() {
   const [amount, setAmount] = useState(1);
@@ -14,16 +17,23 @@ function ProductDetails() {
   };
 
   return (
-    <main>
+    <main className={styles.main}>
       {product && (
         <>
-          <img src={product.image} alt={product.title} />
-          <div>
-            <h1>{product.title}</h1>
-            <p>{product.description}</p>
-            <strong>£{product.price}</strong>
-            <ProductAmountControl amount={amount} setAmount={setAmount} />
-            <button onClick={handleAddToCart}>Add to cart</button>
+          <img className={styles.img} src={product.image} alt={product.title} />
+          <div className={styles.div}>
+            <div className={styles.section}>
+              <h1 className={styles.title}>{product.title}</h1>
+              <Rating rating={product.rating} />
+              <p className={styles.description}>{product.description}</p>
+              <Price price={product.price} />
+            </div>
+            <div className={styles.section}>
+              <ProductAmountControl amount={amount} setAmount={setAmount} />
+              <button className={styles.button} onClick={handleAddToCart}>
+                Add to cart
+              </button>
+            </div>
           </div>
         </>
       )}
