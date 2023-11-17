039cb003f9c793e7b00a7f0d1ca79f03e82daf0c
Elliot Akande <elbo.lynch@gmail.com>
2023-11-17 15:36:40 +0000

    Create Rating component

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
+import StarHalfIcon from "@mui/icons-material/StarHalf";
+import styles from "./Rating.module.css";
 
 function Rating({ rating }) {
   const { rate, count } = rating;
+  const [stars, setStars] = useState([
+    <StarBorderIcon key={0} sx={{ color: "gold" }} />,
+    <StarBorderIcon key={1} sx={{ color: "gold" }} />,
+    <StarBorderIcon key={2} sx={{ color: "gold" }} />,
+    <StarBorderIcon key={3} sx={{ color: "gold" }} />,
+    <StarBorderIcon key={4} sx={{ color: "gold" }} />,
+  ]);
 
-  return <div>{rate} stars</div>;
+  useEffect(() => {
+    if (rate) {
+      const updatedStars = stars.map((star, index) => {
+        const starRate = rate - index;
+
+        if (starRate > 0.75)
+          return <StarIcon key={index} sx={{ color: "gold" }} />;
+        if (starRate > 0.25 && starRate <= 0.75)
+          return <StarHalfIcon key={index} sx={{ color: "gold" }} />;
+        return <StarBorderIcon key={index} sx={{ color: "gold" }} />;
+      });
+
+      setStars(updatedStars);
+    }
+  }, [rate]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.stars}>{stars}</div>
+      <p className={styles.count}>{count}</p>
+    </div>
+  );
 }
 
 const isValidRating = (props, propName, componentName) => {
