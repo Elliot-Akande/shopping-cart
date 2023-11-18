c0585b47184c814d82342a22264156beea6ffef6
Elliot Akande <elbo.lynch@gmail.com>
2023-11-18 19:45:15 +0000

    Add back button

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,20 +1,21 @@
-import { useOutletContext, useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import ProductAmountControl from "../ProductAmountControl/ProductAmountControl";
-import styles from "./ProductDetails.module.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { useEffect, useState } from "react";
+import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import Price from "../Price/Price";
+import ProductAmountControl from "../ProductAmountControl/ProductAmountControl";
 import Rating from "../Rating/Rating";
+import styles from "./ProductDetails.module.css";
 
 function ProductDetails() {
   const [amount, setAmount] = useState(1);
   const { products, cartManager, toggleCart } = useOutletContext();
+  const id = parseInt(useParams().id, 10);
+  const product = products ? products.find((item) => item.id === id) : null;
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-
-  const id = parseInt(useParams().id, 10);
-  const product = products ? products.find((item) => item.id === id) : null;
 
   const handleAddToCart = () => {
     cartManager.modify(id, amount);
@@ -29,10 +30,29 @@ function ProductDetails() {
     setAmount((prev) => (prev > 1 ? prev - 1 : 1));
   };
 
+  const getCategory = () => {
+    switch (product.category) {
+      case "jewelery":
+        return "accessories";
+      case "electronics":
+        return "technology";
+      default:
+        return "clothing";
+    }
+  };
+
+  const handleBackButton = () => {
+    !product ? navigate("/") : navigate(`/products/${getCategory()}`);
+  };
+
   return (
     <main className={styles.main}>
+      <button className={styles.back} onClick={handleBackButton}>
+        <ArrowBackIosIcon className={styles.arrow} aria-hidden="true" />
+        {getCategory().toUpperCase()}
+      </button>
       {product && (
-        <>
+        <div className={styles.container}>
           <img className={styles.img} src={product.image} alt={product.title} />
           <div className={styles.div}>
             <div className={styles.section}>
@@ -53,7 +73,7 @@ function ProductDetails() {
               </button>
             </div>
           </div>
-        </>
+        </div>
       )}
     </main>
   );
