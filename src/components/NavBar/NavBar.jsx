de606d451bc1c4170ea90950edfd749fdf6547c8
Elliot Akande <elbo.lynch@gmail.com>
2023-11-18 12:34:52 +0000

    Add cart button functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,33 +2,39 @@ import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 
-function NavBar() {
+function NavBar({ toggleCart }) {
   return (
-    <nav className={styles.nav}>
-      <Link className={styles.logo} to="/">
-        AKANDE
-      </Link>
-      <ul className={styles.links}>
-        <li>
-          <Link className={styles.link} to="/products/clothing">
-            CLOTHING
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.link} to="/products/accessories">
-            ACCESSORIES
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.link} to="/products/technology">
-            TECHNOLOGY
-          </Link>
-        </li>
-      </ul>
-      <button className={styles.basket} aria-label="basket">
-        <ShoppingBagOutlinedIcon sx={{ fontSize: 24, color: "#fff" }} />
-      </button>
-    </nav>
+    <>
+      <nav className={styles.nav}>
+        <Link className={styles.logo} to="/">
+          AKANDE
+        </Link>
+        <ul className={styles.links}>
+          <li>
+            <Link className={styles.link} to="/products/clothing">
+              CLOTHING
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} to="/products/accessories">
+              ACCESSORIES
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.link} to="/products/technology">
+              TECHNOLOGY
+            </Link>
+          </li>
+        </ul>
+        <button
+          className={styles.basket}
+          onClick={toggleCart}
+          aria-label="basket"
+        >
+          <ShoppingBagOutlinedIcon sx={{ fontSize: 24, color: "#fff" }} />
+        </button>
+      </nav>
+    </>
   );
 }
 
